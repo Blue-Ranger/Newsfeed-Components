af05074574223ea1b401fe0a6c136abae1e775e1
Blue-Ranger <53351056+Blue-Ranger@users.noreply.github.com>
2020-04-16 21:57:17 -0400

    mini change in arrangement

--- Article/Article.js
+++ Article/Article.js
@@ -173,17 +173,6 @@ const articlesDiv = document.querySelector('.articles')
 data.forEach((article) => {
   articlesDiv.appendChild(createArticle(article))
 })
-//   Hint: You will need to use createElement more than once here!
-
-//   Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
-
-//   Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
-
-//   Step 3: return the entire component.
-
-//   Step 4: Map over the data, creating a component for each object and add each component to the DOM as children of the 'articles' div.
-
-//   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 const newArticleData = {
   title: 'Oh my!',
@@ -197,3 +186,17 @@ const newArticleData = {
 
 const newArt = createArticle(newArticleData)
 articlesDiv.appendChild(newArt)
+
+
+//   Hint: You will need to use createElement more than once here!
+
+//   Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
+
+//   Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
+
+//   Step 3: return the entire component.
+
+//   Step 4: Map over the data, creating a component for each object and add each component to the DOM as children of the 'articles' div.
+
+//   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
+
